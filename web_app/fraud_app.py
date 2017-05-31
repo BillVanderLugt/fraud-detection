fbe7a4f23c2049b3109feb148a488bef42400715
Steve Harshman <sharshman193@gmail.com>
2017-05-31 14:58:59 -0600

    starting the model page

--- web_app/fraud_app.py
+++ web_app/fraud_app.py
@@ -13,5 +13,9 @@ def hello():
 def problem():
     return render_template('problem.html')
 
+@app.route('/model')
+def model():
+    return render_template('model.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
