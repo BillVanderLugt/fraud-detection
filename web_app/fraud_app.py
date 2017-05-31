f56ec7da538ce5a03ad4ef12e6049e143a4e2878
Steve Harshman <sharshman193@gmail.com>
2017-05-31 16:18:41 -0600

    working on feeding app data into model

--- web_app/fraud_app.py
+++ web_app/fraud_app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
+import
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +19,22 @@ def problem():
 @app.route('/model')
 def model():
     return render_template('model.html')
+
+@app.route('/score', methods=['POST'])
+def model_predict():
+    body_length = request.form['body_length']
+    sale_duration2 = request.form['sale_duration2']
+    user_age= request.form['user_age']
+    name_length = request.form['name_length']
+    payee_name = request.form['payee_name']
+    user_type = request.form['user_type']
+    fb_published = request.form['fb_published']
+
+    X = 0
+    with open('../model/final_model.pkl', 'rb') as pickle_file:
+        model = pickle.load(pickle_file)
+    y = model.predict(X)
+    return y
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
