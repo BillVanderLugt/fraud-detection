d634e081ffb2d403828287223d11e9a018729deb
Steve Harshman <sharshman193@gmail.com>
2017-05-31 13:15:22 -0600

    added 'Hello' page and fixed homepage link

--- web_app/fraud_app.py
+++ web_app/fraud_app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/something')
-def something():
-    pass
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
