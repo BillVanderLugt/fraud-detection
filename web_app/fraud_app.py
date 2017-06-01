038449b0411e075c01fea8ad8b9e4759e4da6bb2
Steve Harshman <sharshman193@gmail.com>
2017-06-01 13:42:53 -0600

    adding code from the 'example_app'

--- web_app/fraud_app.py
+++ web_app/fraud_app.py
@@ -3,9 +3,14 @@ import pickle
 import sys
 import build_model
 import psycopg2
+import socket
 from predict import predict_and_store
 
 app = Flask(__name__)
+PORT = 8080
+REGISTER_URL = "http://galvanize-case-study-on-fraud.herokuapp.com/data_point"
+DATA = []
+TIMESTAMP = []
 
 @app.route('/')
 def index():
@@ -23,6 +28,12 @@ def problem():
 def model():
     return render_template('model.html')
 
+@app.route('/score', methods=['POST'])
+def score():
+    DATA.append(json.dumps(request.json, sort_keys=True, indent=4, separators=(',', ': ')))
+    TIMESTAMP.append(time.time())
+    return render_template('score_prompt.html', predicted=y)
+
 @app.route('/score_prompt', methods=['POST'])
 def score():
     body_length = request.form['body_length']
@@ -38,11 +49,34 @@ def score():
     y = predict_and_store(record,model,conn)
     return render_template('score_prompt.html', predicted=y)
 
+@app.route('/check')
+def check():
+    line1 = "Number of data points: {0}".format(len(DATA))
+    if DATA and TIMESTAMP:
+        dt = datetime.fromtimestamp(TIMESTAMP[-1])
+        data_time = dt.strftime('%Y-%m-%d %H:%M:%S')
+        line2 = "Latest datapoint received at: {0}".format(data_time)
+        line3 = DATA[-1]
+        output = "{0}\n\n{1}\n\n{2}".format(line1, line2, line3)
+    else:
+        output = line1
+    return output, 200, {'Content-Type': 'text/css; charset=utf-8'}
+
+def register_for_ping(ip, port):
+    registration_data = {'ip': ip, 'port': port}
+    requests.post(REGISTER_URL, data=registration_data)
+
+
 if __name__ == '__main__':
     # unpickle model before app run to establish 'model' in global namespace
     with open('data/pure_rf_model.pkl', 'rb') as pickle_file:
         model = pickle.load(pickle_file)
     # ...and do the same with psql connection 'conn'
     conn = psycopg2.connect(dbname='eventdata', user='postgres', host='localhost',password='password')
+
+    # Register for pinging service
+    ip_address = socket.gethostbyname(socket.gethostname())
+    print "attempting to register %s:%d" % (ip_address, PORT)
+    register_for_ping(ip_address, str(PORT))
 
     app.run(host='0.0.0.0', port=8080, debug=True)
